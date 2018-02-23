9812827619f8bb6b63f3bf0af1094f30ec6ab934
Simone Radice <simone.radice>
2018-02-23 14:16:38 +0100

    bug fix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import 'reflect-metadata';
-import 'zone.js';
 
 
 import { AppModule } from './app/app.module';
